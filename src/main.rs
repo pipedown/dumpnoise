54a32d260a7d5d472aa3aaf4c4b947e7889586ad
Volker Mische <volker.mische@gmail.com>
2017-04-06 19:55:19 +0200

    Initial commit
    Dumps basic key-values out of a Noise index

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+extern crate clap;
+extern crate noise_search;
+extern crate rocksdb;
+
+use std::io::Write;
+use std::process;
+use std::str;
+
+use clap::{Arg, App};
+use noise_search::index::Index;
+
+fn main() {
+    let matches = App::new("dumpnoise")
+        .version("0.1")
+        .about("Dumps the contents of a Noise instance")
+        .arg(Arg::with_name("noise-directory")
+             .help("The data directory of the Noise instance")
+             .required(true))
+        .get_matches();
+    let noisedir = matches.value_of("noise-directory").unwrap();
+
+    let mut stderr = std::io::stderr();
+    let mut index = Index::new();
+    index.open(noisedir, None).unwrap_or_else(|err| {
+        writeln!(&mut stderr, "Error: {}", err)
+            .expect("Could not write to stderr");
+        process::exit(1);
+    });
+
+    let db = index.rocks.unwrap();
+    println!("Dumping: {}", noisedir);
+
+    for (key, value) in db.iterator(rocksdb::IteratorMode::Start) {
+        let key_string = unsafe { str::from_utf8_unchecked((&key)) }.to_string();
+        let value_type = value[0] as char;
+        let value_as_string = match value_type {
+            'o' => "{}".to_string(),
+            'a' => "[]".to_string(),
+            's' => format!("\"{}\"", unsafe { str::from_utf8_unchecked((&value[1..])) }),
+            'T' => "true".to_string(),
+            'F' => "false".to_string(),
+            'f' => {
+                unsafe {
+                    let array = *(value[1..].as_ptr() as *const [_; 8]);
+                    format!("{}", std::mem::transmute::<[u8; 8], f64>(array))
+                }
+            },
+            'N' => "null".to_string(),
+            _ => format!("Unknown value type ({}): {:?}", value_type, value),
+        };
+        println!("{}:\t{}", key_string, value_as_string);
+    }
+}
